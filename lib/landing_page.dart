d71855a9929f5ed899ab016c9b4ebf26e91d8862
Dapo <odapo138@gmail.com>
2022-04-14 16:47:12 +0100

    enabled web functionality

--- lib/landing_page.dart
+++ lib/landing_page.dart
@@ -17,6 +17,7 @@ class _LandingPageState extends State<LandingPage> {
     }
   }
 
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:portfolio_website/landingPage.dart';
+import 'package:portfolio_website/landing_page.dart';
 
 void main (){
   runApp(const MyApp());
